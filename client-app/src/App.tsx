fe98be1ad61de6beee4f6750730321e9b766b191
Macy Parker <macyvparker@gmail.com>
2021-09-05 12:27:05 -0400

    Fetch activities data from the API to UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  // Use state hook from React
+  const[activities, setActivities] = useState([]);// [nameOfVariabletoStoreState, functionToSetState]
+
+  // Fetch our activities from the API server; use hook Use Effect
+  useEffect(() => { // Provide this with a function
+    axios.get('http://localhost:5000/api/activities').then(response =>{
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, []) // including this empty array (symbolizing dependencies) ensures this only runs once, and not an endless loop
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {/* JS code inside the curly brackets, map accepts a callback for each element in an array so we can do something with it */}
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))} 
+        </ul>
       </header>
     </div>
   );
