1b3d012ee97ba41c6b3cbfc465440387accd2785
Macy Parker <macyvparker@gmail.com>
2021-09-06 12:46:16 -0400

    Mediator defined in BaseAPIController

--- API/Controllers/ActvitiesController.cs
+++ API/Controllers/ActvitiesController.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
-
         // Return a list of our activities
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            // Mediator is inherited from BaseApiController
+            return await Mediator.Send(new List.Query());
         }
 
         // {id} is a root parameter
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,6 +8,12 @@ namespace API.Controllers
     [Route("api/[controller]")] // controller is a placeholder that gets replaced with the name of the controller class name, minus the word controller
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+
+        // protected is available to any derived classes + this class
+        // ??= null coalescing assignment operator
+        // set Mediator to _mediator, but if its null, it'll assign Mediator to whatever is to the right of ??=
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         
     }
 }
